d2637b98ce7f025e7f098b5f7d927f02a2078385
Rendy-ptr <rendyp348@gmail.com>
2025-09-19 19:53:04 +0700

    feat(layouts): create app-layout component and refactor pages to use it for DRY

---
+++ resources/views/components/layouts/app-layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full scroll-smooth">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> {{ $title }} </title>
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="min-h-screen pt-16">
+    {{ $slot }}
+</body>
+
+</html>
